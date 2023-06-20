955e6d3dee3838f34a06b0b512548be9a32b541f
Gunnar Batista <gunnar-batista@hotmail.com>
2023-06-20 08:45:16 -0300

    Alter codes

--- mybooks_client/src/App.vue
+++ mybooks_client/src/App.vue
@@ -1,24 +1,23 @@
 
 <template>
   <div>
-    <h1>My Books</h1>
-
-    <ul style="list-style: none;">
-      <li><router-link to="/">Home</router-link></li>
-      <li> Categorias
-        <ul>
+    <h1 style="justify-content: center; display: flex;">My Books</h1>
+    <ul style="list-style: none;justify-content: center;display: flex; margin: 50px 0;">
+      <li style="margin: 0 20px;"><router-link to="/">Home</router-link></li>
+      <li style="margin: 0 20px;"> Categorias
+        <ul style="list-style: none;">
           <li><router-link to="/categories">Listar Categorias</router-link></li>
           <li><router-link to="/categories/create">Adicionar Categoria</router-link></li>
         </ul>
       </li>
-      <li> Autores
-        <ul>
+      <li style="margin: 0 20px;"> Autores
+        <ul style="list-style: none;">
           <li><router-link to="/authors">Listar Autores</router-link></li>
           <li><router-link to="/authors/create">Adicionar Autores</router-link></li>
         </ul>
       </li>
-      <li> Livros
-        <ul>
+      <li style="margin: 0 20px;"> Livros
+        <ul style="list-style: none;">
           <li><router-link to="/books">Listar Livros</router-link></li>
           <li><router-link to="/books/create">Adicionar Livros</router-link></li>
         </ul>
--- mybooks_client/src/views/home/home.vue
+++ mybooks_client/src/views/home/home.vue
@@ -1,7 +1,12 @@
 <template>
     <div>
-        <h1>Pagina inicial</h1>
+        <h1 style="justify-content: center; display: flex;">Pagina inicial</h1>
+        <h2 style="justify-content: center; display: flex;">Aplicação para cadastro de Livros</h2>
+        <div style="justify-content: center; display: flex;">
+            <img src="../../assets/livro.gif">
+        </div>
     </div>
+    
 </template>
 
 <script setup>
